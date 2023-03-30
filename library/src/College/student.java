7e78b76c3e5e5ad0495bd95c8229fc435a120bf4
Harijjr <113496599+Harijjr@users.noreply.github.com>
2023-03-30 14:52:19 +0530

    Add files via upload

---
+++ library/src/College/student.java
@@ -0,0 +1,33 @@
+package College;
+
+import java.util.Scanner;
+
+// Class
+public class student {
+
+	// Class member variables
+	public String studentName;
+	public String regNum;
+
+	public book borrowedBooks[] = new book[3];
+	public int booksCount = 0;
+
+	// Creating object of Scanner class to
+	// take input from user
+	Scanner input = new Scanner(System.in);
+
+	// Constructor
+	public student()
+	{
+		// Print statement
+		System.out.println("Enter Student Name:");
+
+		// This keywords refers to current instance
+		this.studentName = input.nextLine();
+
+		// Print statement
+		System.out.println("Enter Registration Number:");
+		this.regNum = input.nextLine();
+	}
+}
+
